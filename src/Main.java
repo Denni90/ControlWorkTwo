e380cc7124ba03695dd3134092d65d8bb675c1f4
denis.tsapenko <denis.tsapenko90@yandex.ru>
2024-03-17 10:33:19 +0300

    Create controlwork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            // Чтение файла
+            File file = new File("C://ControlWorkTwo/ControlWorkTwo/input.txt");
+            Scanner scanner = new Scanner(file);
+
+            // Подсчет слов
+            int wordCount = 0;
+            while (scanner.hasNext()) {
+                scanner.next();
+                wordCount++;
+            }
+            System.out.println("Количество слов: " + wordCount);
+
+            // Поиск самого длинного слова
+            scanner = new Scanner(file);
+            String longestWord = "";
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (word.length() > longestWord.length()) {
+                    longestWord = word;
+                }
+            }
+            System.out.println("Самое длинное слово: " + longestWord);
+
+            // Вычисление частоты слов
+            scanner = new Scanner(file);
+            Map<String, Integer> wordFrequency = new HashMap<>();
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+            }
+            System.out.println("Частота слов:");
+            for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
     }
 }
